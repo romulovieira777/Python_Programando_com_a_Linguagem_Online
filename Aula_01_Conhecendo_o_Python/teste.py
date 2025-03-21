bbed81c5c9ae421526d0c8dfa217d7d65593955c
Romulo Vieira <romulo.vieira777@gmail.com>
2025-03-21 09:39:27 -0300

    Conhecendo o Python

---
+++ Aula_01_Conhecendo_o_Python/teste.py
@@ -0,0 +1,17 @@
+print("Hello world")
+
+print(45 + 17)
+
+print(type(62))
+
+print(dir(62))
+
+print(62 + 3)
+
+print('Teste')
+
+print("Novo Teste")
+
+print(dir("Novo Teste"))
+
+print(help("Novo Teste".upper))
