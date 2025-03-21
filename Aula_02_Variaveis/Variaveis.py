59f5b2f3a5783ed6940c30593f075b47c1da7154
Romulo Vieira <romulo.vieira777@gmail.com>
2025-03-21 09:55:32 -0300

    Variáveis

---
+++ Aula_02_Variaveis/Variaveis.py
@@ -0,0 +1,25 @@
+a = 10
+print(a)
+
+print(42)
+
+print(type(42))
+
+print(id(42))
+
+b = 42
+
+print(id(b))
+
+print(3.14)
+
+print("Texto")
+
+c = 3.14
+
+var1 = 'Nome'
+var2 = 'Sobrenome'
+
+print(var1 + var2)
+
+print(dir(var1))
