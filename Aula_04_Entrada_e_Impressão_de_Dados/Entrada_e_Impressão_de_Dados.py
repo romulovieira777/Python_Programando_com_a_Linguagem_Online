1afb7b58d92b7d358e34d778321243764d3a41b0
Romulo Vieira <romulo.vieira777@gmail.com>
2025-03-25 09:39:46 -0300

    Entrada e Impressão de Dados

---
+++ Aula_04_Entrada_e_Impressão_de_Dados/Entrada_e_Impressão_de_Dados.py
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+print(a + 30)
+
+c = int(input("Digite um número inteiro: "))
+d = int(input("Digite um segundo número inteiro: "))
+print(c + d)
+
+print("Imprimindo...")
+print("Imprimindo", "com", "Python")
+print("Imprimindo", "o número", 5)
+print("Imprimindo", "o número", a)
+print("Eu tirei %d na prova" % a)
+print("Eu tirei %s na prova" % "Dez")
+print("Eu tirei %f na prova" % 9.5)
+print("Eu tirei %2f na prova" % 9.5)
+print("Eu tirei %f na prova" % 9.58774873298478923749382)
+print("Teste de %s, para dia %d. Float %.3f" % ("marcadores", 4, 25.3948384))
