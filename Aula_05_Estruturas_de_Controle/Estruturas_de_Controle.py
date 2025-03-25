58513f6b800f77c03e2ecc7c4f5c47aa646d56d6
Romulo Vieira <romulo.vieira777@gmail.com>
2025-03-25 09:50:40 -0300

    Estruturas de Controle

---
+++ Aula_05_Estruturas_de_Controle/Estruturas_de_Controle.py
@@ -0,0 +1,29 @@
+nota1 = 7
+media = 6
+
+if nota1 >= media:
+    print("Aprovado.")
+elif nota1 == media:
+    print("DP")
+else:
+    print("Reprovado.")
+
+nota1 = int(input("Digite a sua nota: "))
+media = 6
+
+if nota1 >= media:
+    print("Aprovado.")
+elif nota1 == media:
+    print("DP")
+else:
+    print("Reprovado.")
+
+for x in range(10):
+    print(x)
+
+a = 5
+b = 20
+
+while a < b:
+    print(a)
+    a = a + 1
