cfde8f96b93205d202afea0fb2ed63091c61bd57
Romulo Vieira <romulo.vieira777@gmail.com>
2025-03-25 09:29:43 -0300

    Operadores

---
+++ Aula_03_Operadores/Operadores.py
@@ -0,0 +1,32 @@
+a = 35
+b = 30
+c = 10
+
+print(a + b + c)
+
+
+x = 42
+y = 100
+print(x + y)
+
+z = 50.50
+print(z / 2)
+print(z // 2)
+print(10 ** 2)
+print(10 ** 1000)
+
+
+x = 10
+y = 20
+print(x > y)
+print(type(False))
+
+x = 10
+print(x >= 10)
+
+y = 15
+z = 10
+
+print(x == z and x < y)
+
+print(not x == z and x < y)
